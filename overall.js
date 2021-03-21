32893c036f270f20b77a34e2eac705defcec236d
Stefanie Nader <stefanie.nader1992@web.de>
2021-03-21 18:19:28 +0100

    standard backend functions

--- overall.js
+++ overall.js
@@ -1,1 +1,37 @@
+/**
+ * URL for backend integration
+ */
+
 setURL('http://stefanie-nader.developerakademie.com/smallest_backend_ever-master');
+
+
+/**
+ * Backend Functions
+ */
+
+/*Definition for backend functions*/
+let users = [];
+
+/**
+ * async function for downloading data from backend to required pages
+ */
+async function init() {
+    await downloadFromServer();
+    conversations = JSON.parse(backend.getItem('conversations')) || [];
+}
+
+/**
+ * saving Conversations to backend
+ */
+function addConversation() {
+    conversations.push('Apulien');
+    backend.setItem('conversations', JSON.stringify(conversations));
+}
+
+/**
+ * deleting a conversation from backend
+ * @param {current user} destination
+ */
+function deleteUser(destination) {
+    backend.deleteItem('conversations');
+  }
