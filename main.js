ca98b7bfceca03e105d5889a4d0ccaaf60f2567e
Marina240490 <marina_schwab@yahoo.de>
2021-03-23 10:33:47 +0100

    faq and main

---
+++ main.js
@@ -0,0 +1,55 @@
+/**
+ * Open Overlay
+ */
+ function openOverlay() {
+    document.getElementsByClassName('modal-container')[0].classList.remove('hidden');
+}
+
+/**
+ * Close Overlay
+ */
+function closeOverlay() {
+    document.getElementsByClassName('modal-container')[0].classList.add('hidden');
+}
+
+
+/**
+ * Post a question
+ */
+let posts = []; 
+let names = []; 
+
+function addPost() {
+    let text = document.getElementById('question').value; 
+    let name = document.getElementById('name').value; 
+
+    posts.push(text); 
+    names.push(name);
+
+    let myposts = document.getElementById('myposts'); 
+
+    myposts.innerHTML = '';  
+
+    for(let i = 0; i < posts.length; i++) {
+
+        myposts.innerHTML += `
+        <div class="post-div">
+            <div>
+                <button class="removebutton" onclick="removePost(' + index + '); addPost();">X</button>
+            </div>
+            <div class="post">
+                <b>${names[i]}</b><br>
+                ${posts[i]}<br>
+            </div>
+        </div>
+    `; 
+    }
+
+    document.getElementById('question').value = ''; 
+    document.getElementById('name').value = ''; 
+}
+
+function removePost(itemi) {
+    posts.splice(itemi, 1); 
+    names.splice(itemi, 1);
+}
