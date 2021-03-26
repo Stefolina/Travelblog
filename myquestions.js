7981e48ca8e561753d6bb4ec902ffccfaf4a8102
Stefanie Nader <stefanie.nader1992@web.de>
2021-03-26 23:06:15 +0100

    drag and drop not ready

--- myquestions.js
+++ myquestions.js
@@ -1,11 +1,6 @@
 /**
  * Showing the questions
  */
-function showQuestions() {
-    document.getElementById('question-area').innerHTML = '';
-    for (let i = 0; i < allQuestions.length; i++) {
-        const element = allQuestions[i];
-        document.getElementById('question-area').innerHTML += `
-    `;
-    }
+function startDragging() {
+  
 }
