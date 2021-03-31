6e0309df96dbc585a658220e54d817aac2c7eb98
Yvonne Amann <yvonne.amann1@gmx.de>
2021-03-31 18:02:44 +0200

    destination changes

---
+++ destinations.js
@@ -0,0 +1,46 @@
+/**
+ * Function for selecting buttons with respective continent 
+ */
+
+ function selectAmerika() {
+     document.getElementById('continents').innerHTML = `
+     <div class="h3-headings"> <h3> Amerika </h3> 
+     <div class="continent-post"> BlaBla </div>
+     </div>`
+    
+ }
+
+ function selectSüdamerika() {
+    document.getElementById('continents').innerHTML = `
+    <div class="h3-headings"> <h3> Südamerika </h3> 
+    <div class="continent-post"> BlaBla </div>
+    </div>`
+}
+
+function selectEuropa() {
+    document.getElementById('continents').innerHTML = `
+    <div class="h3-headings"> <h3> Europa </h3> 
+    <div class="continent-post"> BlaBla </div>
+    </div>`
+}
+
+function selectAsien() {
+    document.getElementById('continents').innerHTML = `
+    <div class="h3-headings"> <h3> Asien </h3> 
+    <div class="continent-post"> BlaBla </div>
+    </div>`
+}
+
+function selectAustralien() {
+    document.getElementById('continents').innerHTML = `
+    <div class="h3-headings"> <h3> Australien </h3> 
+    <div class="continent-post"> BlaBla </div>
+    </div>`
+}
+
+function selectAntarktis() {
+    document.getElementById('continents').innerHTML = `
+    <div class="h3-headings"> <h3> Antarktis</h3> 
+    <div class="continent-post"> BlaBla </div>
+    </div>`
+}
